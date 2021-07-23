00d55fc821d223f82d64e33e718427c748d3bc00
Michael <micklekoltsov@gmail.com>
2021-07-23 17:28:32 +0300

    Login form fields added

--- front/src/components/LoginForm/LoginForm.tsx
+++ front/src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,8 @@
-import { Field, Form } from 'react-final-form';
+import { Grid, makeStyles, Paper, Theme } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import { GridSize } from '@material-ui/core';
+import { ReactNode } from 'react';
+import { Form } from 'react-final-form';
 import { ButtonTypes } from '../Header/Header';
 
 interface LoginProps {
@@ -14,29 +18,122 @@ interface FormTypes {
   password: string;
 }
 
-export const LoginForm = ({setModal}: LoginProps) => {
+interface FormField {
+  size: GridSize;
+  field: ReactNode;
+}
+
+const useStyles = makeStyles((theme: Theme) =>({
+  formWrapper: {
+    margin: "1vh",
+  }
+}))
+
+export const LoginForm = ({ setModal }: LoginProps) => {
+  const classes = useStyles();
   const onSubmit = () => {}; // TODO onSubmit func
+
+  const formFields: FormField[] = [
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="text"
+          label="Имя"
+          name="firstName"
+          margin="none"
+          required={true}
+        />
+      ),
+    },
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="text"
+          label="Фамилия"
+          name="lastname"
+          margin="none"
+          required={false}
+        />
+      ),
+    },
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="text"
+          label="Никнейм"
+          name="username"
+          margin="none"
+          required={true}
+        />
+      ),
+    },
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="email"
+          label="E-mail"
+          name="email"
+          margin="none"
+          required={true}
+        />
+      ),
+    },
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="text"
+          label="Город"
+          name="city"
+          margin="none"
+          required={false}
+        />
+      ),
+    },
+    {
+      size: 12,
+      field: (
+        <TextField
+          type="password"
+          label="Пароль"
+          name="city"
+          margin="none"
+          required={true}
+        />
+      ),
+    },
+  ];
+
   return (
     <Form<FormTypes>
       onSubmit={onSubmit}
       render={({ handleSubmit, form, submitting, pristine, values }) => (
         <form onSubmit={handleSubmit} noValidate>
-          <div className="form">
+          <div className={classes.formWrapper}>
             <span>Вход</span>
-            <Field name="username">
-              <div>
-                <label>Никнейм</label>
-                <input type="text" placeholder="Никнейм" />
-              </div>
-            </Field>
-            <Field name="password">
-              <div>
-                <label>Пароль</label>
-                <input type="text" placeholder="Пароль" />
-              </div>
-            </Field>
+            <Paper>
+              <Grid
+                container
+                direction="column"
+                alignItems="flex-start"
+                justifyContent="center"
+                spacing={2}
+              >
+                {formFields.map((item, idx)=>(
+                  <Grid item xs={item.size} key={idx}>
+                    {item.field}
+                  </Grid>
+                ))}
+              </Grid>
+              <Grid>
+                <button type="submit">Войти</button>
+              </Grid>
+            </Paper>
           </div>
-          <button type="submit">Войти</button>
         </form>
       )}
     />
