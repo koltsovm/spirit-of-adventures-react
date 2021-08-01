6ed614ffada2f0ef3adad58bf37b72a7b3c80947
Michael <micklekoltsov@gmail.com>
2021-08-01 17:53:28 +0300

    Sign In Redux

--- client/src/redux/actionCreators/userActions.ts
+++ client/src/redux/actionCreators/userActions.ts
@@ -10,17 +10,77 @@ export const login =
     password: string
   ): ThunkAction<void, UserState, unknown, SetUserAction> =>
   async (dispatch) => {
-    const response = await fetch('/api/v1/auth/login', {
+    const response = await fetch('/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        body: JSON.stringify({
-          username,
-          email,
-          firstname,
-          password,
-        }),
       },
+      body: JSON.stringify({
+        username,
+        email,
+        firstname,
+        password,
+      }),
+    });
+
+    if (response.status === 200) {
+      const result = await response.json();
+
+      const {
+        routes,
+        createdRoutes,
+        lastname,
+        firstname,
+        phone,
+        about,
+        avatar,
+        id,
+        email,
+        username,
+        token,
+      } = result;
+
+      localStorage.setItem('token', token);
+
+      dispatch({
+        type: Types.SET_USER,
+        payload: {
+          routes,
+          createdRoutes,
+          lastname,
+          firstname,
+          phone,
+          about,
+          avatar,
+          id,
+          email,
+          username,
+        },
+      });
+    }
+  };
+
+export const signUp =
+  (
+    firstName: string,
+    lastname: string,
+    username: string,
+    email: string,
+    city: string,
+    password: string
+  ): ThunkAction<void, UserState, unknown, SetUserAction> =>
+  async (dispatch) => {
+    const response = await fetch('/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName,
+        lastname,
+        username,
+        email,
+        city,
+        password,
+      }),
     });
 
     if (response.status === 200) {
