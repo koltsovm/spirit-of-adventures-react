57064e5ecf63858c03e91fac547e38da9ac3ef63
Mikhail Koltsov <m.kolcov@altuera.com>
2023-07-01 22:44:44 +0300

    feat: add basic general app actions

---
+++ client/src/redux/actions/appActions.ts
@@ -0,0 +1,22 @@
+import { AppActions, AppTypes } from "../types/appTypes";
+
+export function setIsAuthentificated(b: boolean): AppActions {
+  return {
+    type: AppTypes.SET_IS_AUTHENTIFICATED,
+    payload: b,
+  }
+}
+
+export function setIsLoginError(b: boolean): AppActions {
+  return {
+    type: AppTypes.SET_IS_LOGIN_ERROR,
+    payload: b,
+  }
+}
+
+export function setLanguage(language: string | null): AppActions {
+  return {
+    type: AppTypes.SET_LANGUAGE,
+    payload: language,
+  }
+}
